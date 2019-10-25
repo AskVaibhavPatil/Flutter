7e6996876d726c5f7d9d31cb4c3fa8108105e1ab
Vaibhav Patil <6760119+vaibhavpatilai@users.noreply.github.com>
2019-10-25 00:59:11 -0400

    Height adjusted
    Height adjusted

--- flutter_expense_app/lib/widgets/transaction_list.dart
+++ flutter_expense_app/lib/widgets/transaction_list.dart
@@ -13,7 +13,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 250,
+      height: 375,
       child: transactions.isEmpty ? Column(
         children: <Widget>[
           Text(
